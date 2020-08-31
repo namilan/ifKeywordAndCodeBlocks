c7979e3bbfe99d1f28b2dc423b9a30c6896d99ff
namilan <nichole.milan@gmail.com>
2020-08-31 19:15:53 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 public class Main {
 	
 	public static void main (String[] args) {
+		boolean gameOver = true; 
+		int score = 5000;
+		int levelCompleted = 5;
+		int bonus = 100;
+		
+		if (score==4000) 
+			System.out.println(" Your score was 5000.");
+		System.out.println(" This was executed");
+		
+		
+		
 		
 	}
 
