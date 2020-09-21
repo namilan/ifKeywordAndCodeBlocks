65a651396b8cc0364d8271bdbadfc9f86fa4450b
namilan <nichole.milan@gmail.com>
2020-09-21 19:35:08 -0400

    challenge added

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ public class Main {
 	
 	public static void main (String[] args) {
 		boolean gameOver = true; 
+		boolean secondScore = true;
 		int score = 800;
+		int secondScore1 = 10000;
 		int levelCompleted = 5;
+		int secondScoreLevel = 8;
 		int bonus = 100;
+		int secondBonus = 200;
 		
 //		if (score < 5000 && score > 1000) {
 //			System.out.println("Your score was less than 5000 but greater than 1000");
@@ -17,10 +21,22 @@ public class Main {
 //		
 		// code blocks are optional. however, if you have one line, you should use code blocks. 
 		
-		if (gameOver == true) {
+		if (gameOver) {
 			int finalScore = score + ( levelCompleted * bonus );
 			System.out.println("Your final score was " + finalScore);
 		}
+		
+		if (secondScore) {
+			int finalScoreSecond = secondScore1 + ( secondScoreLevel * secondBonus );
+			System.out.println("Second place score was " + finalScoreSecond);
+		}
+		
+		// print out a second score on the screen with the following
+		// score set to 10000
+		// levelCompleted set to 8
+		// bonus set to 200
+		// but make sure the firsdt printout above sill displays as well. 
+		
 		
 	}
 
